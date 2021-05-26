1ca8caadd1ac5249b2d218482e6966551c4de66f
Alex <contact@wisemouse.ca>
2021-05-25 23:57:03 -0400

    added conditions & output logic

--- js/script.js
+++ js/script.js
@@ -10,12 +10,28 @@ const personalMovieDB = {
     private: false
 };
 
-const   a = prompt('One of the last movies you have watched?', ''),
-        b = prompt('How will you score it?', ''),
-        c = prompt('One of the last movies you have watched?', ''),
-        d = prompt('How will you score it?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+for (let i = 0; i < 2; i++){
+    const   a = prompt('One of the last movies you have watched?', ''),
+        b = prompt('How will you score it?', '');
+
+        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+            personalMovieDB.movies[a] = b;
+            console.log('done');
+        } else {
+            console.log('error');
+            i--;
+        }     
+}
+
+if (personalMovieDB.count < 10 && personalMovieDB.count != 0) {
+    console.log("You have watched too few movies");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30){
+    console.log("You're a classic moviegoer");
+} else if (personalMovieDB.count >= 30){
+    console.log("You're a real movie buff");
+} else {
+    console.log('Some error happend :(');
+}
 
 console.log(personalMovieDB);
