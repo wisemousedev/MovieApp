f34844c3b7dc558a2b601cb44511a2811f3f39c3
Alex <contact@wisemouse.ca>
2021-05-26 23:00:28 -0400

    Convert app logic into functions. Added function to ask and save movie genre

--- js/script.js
+++ js/script.js
@@ -1,9 +1,19 @@
 "use strict";
 
-const numberPfFilms = +prompt("How many movies you have watched?", "");
+let numberOfFilms;
+
+function start() {
+    numberOfFilms = +prompt("How many movies you have watched?", "");
+
+    while(numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt("How many movies you have watched?", "");
+    }
+}
+
+start();
 
 const personalMovieDB = {
-    count: numberPfFilms,
+    count: numberOfFilms,
     movies: {},
     actors: {},
     genres: [],
@@ -11,27 +21,51 @@ const personalMovieDB = {
 };
 
 
-for (let i = 0; i < 2; i++){
-    const   a = prompt('One of the last movies you have watched?', ''),
-        b = prompt('How will you score it?', '');
-
-        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-            personalMovieDB.movies[a] = b;
-            console.log('done');
-        } else {
-            console.log('error');
-            i--;
-        }     
+function saveMyMovies() {
+    for (let i = 0; i < 2; i++){
+        const   a = prompt('One of the last movies you have watched?', ''),
+            b = prompt('How will you score it?', '');
+    
+            if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+                personalMovieDB.movies[a] = b;
+                console.log('done');
+            } else {
+                console.log('error');
+                i--;
+            }     
+    }
 }
 
-if (personalMovieDB.count < 10 && personalMovieDB.count != 0) {
-    console.log("You have watched too few movies");
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30){
-    console.log("You're a classic moviegoer");
-} else if (personalMovieDB.count >= 30){
-    console.log("You're a real movie buff");
-} else {
-    console.log('Some error happend :(');
+
+saveMyMovies();
+
+
+
+function identifyViewerType() {
+    if (personalMovieDB.count < 10 && personalMovieDB.count != 0) {
+        console.log("You have watched too few movies");
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30){
+        console.log("You're a classic moviegoer");
+    } else if (personalMovieDB.count >= 30){
+        console.log("You're a real movie buff");
+    } else {
+        console.log('Some error happend :(');
+    }
 }
 
-console.log(personalMovieDB);
+identifyViewerType();
+
+function showMoviesDB(hidden) {
+    if (!hidden) {
+        console.log(personalMovieDB);
+    }
+}
+
+showMoviesDB(personalMovieDB.private);
+
+function writeYourGenres() { 
+    for (let i = 1; i <= 3; i++) {
+        personalMovieDB.genres[i-1] = prompt(`What is your favorite genre at number ${i}?`);
+    }   
+}
+writeYourGenres();
